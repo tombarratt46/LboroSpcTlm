52de7a74c9e63ee9e5a9f0ff5665e3f53cd904b5
Tom Barratt <tom_barratt@outlook.com>
2022-11-14 20:46:53 +0000

    Added: Reading from BMP280

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,50 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
-
+#include <Adafruit_BMP280.h>
 
 // 84 bytes
 struct reading
 {
+  // Magnetic Field Strength Vector (uT)
   //12 bytes
   float magnetometer_x;
   float magnetometer_y;
   float magnetometer_z;
 
+  // Acceleration Vector (m/s^2)
   //12 bytes
   float accelerometer_x;
   float accelerometer_y;
   float accelerometer_z;
 
+  // Angular Velocity Vector (rad/s)
   //12 bytes
   float gyroscope_x;
   float gyroscope_y;
   float gyroscope_z;
 
+  // Gravity Vector (m/s^2)
   //12 bytes
   float gravity_x;
   float gravity_y;
   float gravity_z;
 
-  //32 bytes
+  // Absolute Orientation Quaterion
+  //16 bytes
   double orientation_w;
   double orientation_x;
   double orientation_y;
   double orientation_z;
 
+  // Vehicle on time in microseconds
   // 4 bytes
   unsigned long VOT;
 
+  // 12 bytes
+  float temperature; //Kelvin
+  float pressure; //Pa
+  float altitude; // Meters (from AGL)
 };
 
 
@@ -44,33 +54,70 @@ struct reading
 // Linear Acceleration 100Hz
 // Magnetic Field strength 20Hz
 // Temperature 1Hz
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+Adafruit_BNO055 bno = Adafruit_BNO055();
+Adafruit_BMP280 bmp = Adafruit_BMP280();
 
 unsigned long BNO055_sample_rate = 100;
+float ground_pressure;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // blink once repeatedly if bno fails to start
   if (!bno.begin())
   {
     while (1) {
       digitalWrite(LED_BUILTIN, HIGH);
       delay(100);
       digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    };
+  }
+  // blink twice repeatedly if bmp fails to start
+  if (!bmp.begin())
+    {
+    while (1) {
+      digitalWrite(LED_BUILTIN, HIGH);
       delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
     };
   }
 
+  // Approx reading @ 26.3hz
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, // Operating Mode
+                  Adafruit_BMP280::SAMPLING_X2, // Temperature sampling mode
+                  Adafruit_BMP280::SAMPLING_X16, // Pressure sampling mode
+                  Adafruit_BMP280::FILTER_X16, // Filtering mode
+                  Adafruit_BMP280::STANDBY_MS_1 // Standby time
+                  );
+  ground_pressure = bmp.readPressure();
   delay(500);
 }
 
 void loop() {
+  reading current_reading;
+
+  getMagnetometer(&current_reading);
+  getAccelerometer(&current_reading);
+  getGyroscope(&current_reading);
+  getGravity(&current_reading);
+  getOrientation(&current_reading);
+  getTemperature(&current_reading);
+  getPressure(&current_reading);
+  getAltitude(&current_reading);
+  current_reading.VOT = micros();
+
 
   delay(1.0 / BNO055_sample_rate * 1000.0);
 }
 
 // Raw data from magnetometer 
-void getMagnetometerFromEvent(reading* current_reading){
+void getMagnetometer(reading* current_reading){
   sensors_event_t event;
   bno.getEvent(&event, Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
@@ -81,7 +128,7 @@ void getMagnetometerFromEvent(reading* current_reading){
 }
 
 // Raw data from magnetometer 
-void getAccelerometerFromEvent(reading* current_reading){
+void getAccelerometer(reading* current_reading){
   sensors_event_t event;
   bno.getEvent(&event, Adafruit_BNO055::VECTOR_ACCELEROMETER);
 
@@ -91,7 +138,7 @@ void getAccelerometerFromEvent(reading* current_reading){
 }
 
 // Raw data from magnetometer 
-void getGyroscopeFromEvent(reading* current_reading){
+void getGyroscope(reading* current_reading){
   sensors_event_t event;
   bno.getEvent(&event, Adafruit_BNO055::VECTOR_GYROSCOPE);
 
@@ -117,4 +164,18 @@ void getOrientation(reading* current_reading){
   current_reading->orientation_x = quat.x();
   current_reading->orientation_y = quat.y();
   current_reading->orientation_z = quat.z();
+}
+
+
+void getTemperature(reading* current_reading){
+  current_reading->temperature = bmp.readTemperature() + 273.15;
+}
+
+void getPressure(reading* current_reading){
+  current_reading->pressure = bmp.readPressure();
+  
+}
+
+void getAltitude(reading* current_reading){
+  current_reading->altitude = bmp.readAltitude(ground_pressure / 100);
 }
